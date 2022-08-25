69678d3fe868d9e94c231a3b7fd4bd96b87cdf05
daneykingsley <daneykingsley@gmail.com>
2022-08-25 11:39:27 +0530

    added 2 method

--- src/test/java/org/aug24/Sample.java
+++ src/test/java/org/aug24/Sample.java
@@ -1,5 +1,16 @@
 package org.aug24;
 
 public class Sample {
+	
+	private void method1() {
+		System.out.println("1");
+	}
+	private void method2() {
+		System.out.println("2");
+	}
+	private void method3() {
+		System.out.println("3");
+	}
+	
 
 }
